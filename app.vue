e46ecb79c21f052c5d5f3e65611b9d67aed5290f
Arman Amini <armanwithamini@gmail.com>
2024-01-11 23:47:28 +0330

    init app

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card flex justify-center">
+    KL
+    <Sidebar v-model:visible="visible" header="Sidebar">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </Sidebar>
+    <Button icon="pi pi-arrow-right" @click="visible = true" />
+  </div>
+</template>
+
+<script setup>
+import Sidebar from 'primevue/sidebar';
+
+import { ref } from "vue";
+
+const visible = ref(false);
+</script>
