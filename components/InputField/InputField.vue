d7e8025e3ada5e98d55928a534c2268ffaabd955
Arman Amini <armanwithamini@gmail.com>
2024-01-13 11:06:06 +0330

    add inputs for registering

---
+++ components/InputField/InputField.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="text"
+    class="border-2 border-gray-300 bg-white text-gray-700 rounded-md py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+    :placeholder="props.placeholder"
+    required
+  />
+</template>
+
+<script setup>
+const props = defineProps(["placeholder"]);
+</script>
+
+<style>
+input:focus:invalid {
+  border-color: red;
+  box-shadow: 0 0 0 1px red; /* Optional: adds a red shadow for better visibility */
+}
+</style>
--- pages/Students.vue
+++ pages/Students.vue
@@ -1,7 +1,36 @@
 <template>
-  <StudentsDataTable></StudentsDataTable>
+  <div class="my-5 mx-auto">
+    <button
+      class="bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded mb-2"
+      @click="isAddStudentOpen = true"
+    >
+      ثبت نام کردن هنرآموز
+    </button>
+    <div
+      v-if="isAddStudentOpen"
+      class="flex flex-wrap gap-x-4 rounded border border-gray-200 dark:border-gray-700 p-4 mb-4"
+    >
+      <div>
+        <span> نام: </span>
+        <InputField />
+      </div>
+      <div>
+        <span> نام خانوادگی: </span>
+        <InputField />
+      </div>
+    </div>
+    <StudentsDataTable></StudentsDataTable>
+  </div>
 </template>
 
 <style>
+div {
+  direction: rtl;
+}
 /* Add any additional styling here */
 </style>
+
+
+<script setup>
+const isAddStudentOpen = ref(false);
+</script>
